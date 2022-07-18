e785acb00ef71e3ccc9b741aefcc4b73f2f9af0e
mdmanik74 <manikmhamud74@outlook.com>
2022-07-18 20:28:24 +0600

    Python boolean and operators

---
+++ Python Booleans and operators.py
@@ -0,0 +1,39 @@
+
+name="Manik"
+age = 25
+print("%s is %d years old" % (name,age))
+#Boolean Values
+
+print(10>9)
+print(10==9)
+print(10<9)
+
+a=200
+b=100
+if b>a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+    #python operators
+
+    #arithmetic
+
+    new=50
+    new2=50
+    print(new+new2)
+
+    new = 50
+    new2 = 6
+    print(new / new2)
+    new = 50
+    new2 = 6
+    print(new // new2)
+
+    #assinment
+
+    ass=20
+    ass2=5
+    result=ass+ass2
+    result +=ass2
+    print(result)
