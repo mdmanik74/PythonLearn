d3bc70290e2b97fd71c85b5f339f81eeff111c46
mdmanik74 <manikmhamud74@outlook.com>
2022-07-18 18:48:19 +0600

    python string ,numbers, Datatype

---
+++ data_type.py
@@ -0,0 +1,47 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+x = 1
+y = 35
+z = 3.255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+
+#Python Strings
+text="python is the language"
+print("free" in text)
+
+#Python - Modify Strings
+a = "Hello, World!"
+print(a.upper())
+a = "Hello, World!"
+print(a.lower())
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#Escape Sequence
+
+print("hello \n bangladesh")
+
+#data type
+
+x={1,2,3}
+result=type(x)
+print(result)
+
+y=3+7j
+output=type(y)
+print(output)
