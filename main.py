6789240c2ee48cdd9a118bf2c5e80497060780eb
mdmanik74 <manikmhamud74@outlook.com>
2022-07-18 17:33:08 +0600

    python print, comments, variables

---
+++ main.py
@@ -0,0 +1,25 @@
+
+#1st topic
+print("Today start learning python")
+
+#2nd topic
+
+if 5>2:
+    print("five is greater than two")
+
+#3rd topic single comment
+'''
+multiple line comment
+'''
+
+#variable
+
+myName="Md manik"
+my_name=" md Manik"
+print(myName+my_name)
+
+#multipale values
+
+a,b,c,d ="Orange", "Mango", "banana", "Cherry"
+
+print(a,b,c,d)
