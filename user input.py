08fec37b05c72ae71d1c98a0a41e2242de076de6
mdmanik74 <manikmhamud74@outlook.com>
2022-07-19 12:44:54 +0600

    User input keyboard, int, flot, output

---
+++ user input.py
@@ -0,0 +1,24 @@
+'''
+number= int(input('Enter your first number : '))
+number2 =int(input('Enter your second number: '))
+reuslt=number+number2
+print(reuslt)
+'''
+
+'''
+#name = input('What is your name?\n')
+#print(name)
+
+# input
+num1 = float(input())
+num2 = float(input())
+
+# printing the sum in float
+print(num1 + num2)
+'''
+#multiple inputs
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+print()
