01b48719c0fbbcb40cf446941ecacdbdbd2e4353
mdmanik74 <manikmhamud74@outlook.com>
2022-07-19 13:48:33 +0600

    python lists and access list

---
+++ python list.py
@@ -0,0 +1,18 @@
+
+
+'''
+mylist= ["apple", "banana", "cherry"]
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+'''
+
+#thislist = ["apple", "banana", "cherry"]
+#print(thislist[1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "me3on", "mango"]
+print(thislist[2:5])
