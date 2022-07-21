fed2fbb0ec4d74b0e17c7834ed9c1860d7b754eb
mdmanik74 <manikmhamud74@outlook.com>
2022-07-21 21:07:06 +0600

    List item change list, add list, remove list

---
+++ lists more.py
@@ -0,0 +1,32 @@
+#change_list
+
+'''
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+#change_list2
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+#addItem
+
+thislist=["apple", "banana", "cherry"]
+thislist.append("manik")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+'''
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
